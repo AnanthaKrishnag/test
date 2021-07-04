22cc4fcb5cfbc8dec864cc3ff3150d6bd97e7c90
AnanthaKrishnag <athraya24@gmail.com>
2021-07-04 20:00:34 +0530

    last update

--- github test project/Assets/Scenes/testscript.cs
+++ github test project/Assets/Scenes/testscript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test_script : MonoBehaviour
+public class testscript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
